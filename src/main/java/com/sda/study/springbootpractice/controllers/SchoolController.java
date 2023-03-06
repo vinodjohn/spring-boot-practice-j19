e81bf6493438058fe1083576ec722cee9693434d
Vinod John <vinodjohn56@gmail.com>
2023-03-06 19:43:24 +0200

    Create school implemented

--- src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
+++ src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
@@ -2,14 +2,12 @@ package com.sda.study.springbootpractice.controllers;
 
 import com.sda.study.springbootpractice.exceptions.CourseNotFoundException;
 import com.sda.study.springbootpractice.exceptions.SchoolNotFoundException;
+import com.sda.study.springbootpractice.models.School;
 import com.sda.study.springbootpractice.services.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
@@ -48,6 +46,30 @@ public class SchoolController {
             return "redirect:/school";
         } catch (SchoolNotFoundException | CourseNotFoundException e) {
             return handleException(redirectAttributes, e);
+        }
+    }
+
+    //To show the create school form page
+    @GetMapping("/create")
+    public String showCreateSchoolPage(@ModelAttribute("school") School school,
+                                       @ModelAttribute("message") String message,
+                                       @ModelAttribute("messageType") String messageType) {
+        return "school/create-school";
+    }
+
+    // Called when we press submit button in the create-school form
+    @PostMapping
+    public String createSchool(School school, RedirectAttributes redirectAttributes) {
+        try {
+            School searchSchool = schoolService.findSchoolByName(school.getName());
+            redirectAttributes.addFlashAttribute("message", String.format("School(%s) already exists!", school.getName()));
+            redirectAttributes.addFlashAttribute("messageType", "error");
+            return "redirect:/school/create-school";
+        } catch (SchoolNotFoundException e) {
+            schoolService.createSchool((school));
+            redirectAttributes.addFlashAttribute("message", String.format("School(%s) has been created successfully!", school.getName()));
+            redirectAttributes.addFlashAttribute("messageType", "success");
+            return "redirect:/school";
         }
     }
 
